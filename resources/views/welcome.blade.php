b6b85b2b0cd0e88b0a5c802cda96fb3abaeb8219
Valentin <valentin.urbine967@gmail.com>
2023-06-08 23:20:56 -0300

    23:20 08-06-2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,13 +15,13 @@
 		<link rel="stylesheet" href="css/style.css">
     </head>
     <body class="antialiased">
-        <div class="container">
+        <form class="container " >
             <h1>formulario de inscripcon</h1>
+            <h2>DATOS PERSONALES</h2>
             <div class="row">
 
-                <form action="" class="form-control" style="border: 1px solid #0000003f; width:355px">
+                <div action="" class="form-control" style="border: 1px solid #0000003f; width:355px">
                     <div class="" style="display: flex; justify-content: space-between; align-items:center;">
-    
     
                         <label class="custom-file-upload" style="width:160px; margin-right:30px;">
                             <input type="file" class="btn btn-secondary" name="gorra" onchange="previewImage(event, '#imgPreview')" required>
@@ -34,101 +34,135 @@
                             </a>
                         </div>
     
+    
                         
                     </div>
                     
                     <label for="">Nombre/s</label>
-                    <input class="form-control" type="text">
+                    <input required class="form-control" type="text">
                     <label for="">Apellido/s</label>
-                    <input class="form-control mb-2" type="text">
-                    
+                    <input required class="form-control mb-2" type="text">
+
                     <div class="d-block m-auto">
                         <label for=""><strong>Sexo</strong>:</label>
                         <label for="">Masculino</label>
-                        <input type="radio" name="sexo" id="">
+                        <input required type="radio" name="sexo" id="">
         
                         <label for="">Femenino</label>
-                        <input  type="radio" name="sexo" id="">
+                        <input required  type="radio" name="sexo" id="">
                         
                         <label for="">Otro</label>
-                        <input type="radio" name="sexo" id=""><br>
+                        <input required type="radio" name="sexo" id=""><br>
                     </div>
-    
-                    <select name="" id="" class="form-control my-2">
-                        <option value=""selected hidden>Lugar de nacimiento</option>
-                        <option value="">San Nicolas, Buenos Aires.</option>
-                        <option value="">Ramallo, Buenos Aires.</option>
-                        <option value="">Rosario, Santa fe.</option>
-                        <option value="">Villa Constitucion, Santa fe.</option>
-                    </select>
-                    <input class="form-control mt-2" type="text" placeholder="DNI (12.345.678)">
+
+                    <input required class="form-control mt-2" type="number" placeholder="CUIL (20-12.345.678-1)">
+                    <input required class="form-control mt-2" type="number" placeholder="DNI (12.345.678)">
                     <div class="d-grid mt-2">
                         <button class="btn btn-primary">INSCRIBIRME</button>
                     </div>
     
-                </form>
+                </div>
     
-                <form action="" class="form-control mx-3" style="border: 1px solid #0000003f; width:355px">
+                <div action="" class="form-control mx-3" style="border: 1px solid #0000003f; width:355px">
 
-                    <input class="form-control mt-2" type="text" placeholder="CUIL (20-12.345.678-1)">
                     <div class="d-flex col-12 mt-2 mb-2">
                         <label for="" class="d-block col-5 mt-2">Fecha Nacimiento</label>
-                        <input type="date" name="" class="form-control" id="">
+                        <input required type="date" name="" class="form-control" id="">
                     </div>
-                    
-                  
-    
-                </form>
-    
-                <form action="" class="form-control" style="border: 1px solid #0000003f; width:355px">
-                    <div class="" style="display: flex; justify-content: space-between; align-items:center;">
-    
-    
-                        <label class="custom-file-upload" style="width:160px; margin-right:30px;">
-                            <input type="file" class="btn btn-secondary" name="gorra" onchange="previewImage(event, '#imgPreview')" required>
-                            Subir foto
-                        </label>
-                        <div class="container d-flex justify-content-center" style="height: 130px; width:130px: display:flex; justify-content: center; box-shadow: 0px 0px 1px #000; background:#fff">
-                            <a href="#" type="button" >
-                                
-                                <img id="imgPreview" style="height: 100%; width:100%;">
-                            </a>
-                        </div>
-    
-                        
+                    <div class="d-flex col-12 mt-2 mb-2">
+                        <input required type="text" name="" class="form-control mx-1 ml-0" placeholder="Lugar Nacimiento">
+                        <input required type="text" name="" class="form-control" placeholder="Provincia Nacimiento">
                     </div>
-                    
-                    <label for="">Nombre/s</label>
-                    <input class="form-control" type="text">
-                    <label for="">Apellido/s</label>
-                    <input class="form-control mb-2" type="text">
-                    
-                    <div class="d-block m-auto">
-                        <label for=""><strong>Sexo</strong>:</label>
-                        <label for="">Masculino</label>
-                        <input type="radio" name="sexo" id="">
-        
-                        <label for="">Femenino</label>
-                        <input  type="radio" name="sexo" id="">
+
+                    <select name="" id="" class="form-control my-2">
+                        <option value=""selected hidden>Elija la carrera</option>
+                        <option value="">TS - Analista de sistemas</option>
+                        <option value="">TS - Recursos Humanos</option>
+                        <option value="">TS - Sistemas de Inform. Contable</option>
+                        <option value="">TS - Mantenimiento industrial</option>
+                        <option value="">TS - Logistica</option>
+                    </select>
+                    <div class="">
+                        <label for=""><strong>Nacionalidad: </strong></label><br>
+                        <label for="">Argentina</label>
+                        <input required type="radio" name="nacionalidad" id="">
                         
                         <label for="">Otro</label>
-                        <input type="radio" name="sexo" id=""><br>
+                        
+                        <input required type="radio" name="nacionalidad" id="">
+                        <input required type="text" class="col-4" name="nacionalidad">
                     </div>
-    
-                    <select name="" id="" class="form-control mt-2">
-                        <option value=""selected hidden>Lugar de nacimiento</option>
-                        <option value="">San Nicolas, Buenos Aires.</option>
-                        <option value="">Ramallo, Buenos Aires.</option>
-                        <option value="">Rosario, Santa fe.</option>
-                        <option value="">Villa Constitucion, Santa fe.</option>
+                    <div class="mt-2">
+                        <input required type="text" class="col-4" placeholder="Domicilio">
+                        <input required type="text" class="col-3" placeholder="Barrio">
+                        <input required type="text" class="col-4" placeholder="Ciudad">
+                    </div>
+                    <select name="" id="" class="my-2">
+                        <option value=""selected hidden>Elija su Provincia</option>
+                        <option value="">Buenos Aires</option>
+                        <option value="">Santiago del estero</option>
+                        <option value="">Santa fe</option>
+                        <option value="">Entre Ríos</option>
                     </select>
+                    <input required type="text" class="d-inline col-4" placeholder="Código Postal">
+                    
+                    <input required class="form-control mt-2" type="email" placeholder="Ingrese Correo Electrónico">
+                    <input required class="form-control mt-2" type="email" placeholder="Vuelva a ingresar Correo Electrónico">
+                </div>
+       
+                <div action="" class="form-control mx-3" style="border: 1px solid #0000003f; width:355px">
+                    <div class="mb-2">
+                        <select name="" id=" "class="form-control ">
+                            <option value="" selected hidden>Elija su estado Civil</option>
+                            <option value="">Soltero</option>
+                            <option value="">Casado</option>
+                            <option value="">Prefiero no decirlo</option>
+                            
+                        </select>
 
-    
-                </form>
+                    </div>
+                    <label for="" class="">¿Tiene Hijos?: </label>
+                    <label for="">Sí</label>
+                    <input required type="radio" name="respuesta_hijos" id="">
+                    <label for="">No</label>
+                    <input required type="radio" name="respuesta_hijos" id="">
+
+                    <label for="">¿Tiene familiares a cargo?</label>
+                    <label for="">Sí</label>
+                    <input required type="radio" name="familiares_a_cargo" id="">
+                    <label for="">No</label>
+                    <input required type="radio" name="familiares_a_cargo" id="">
+
+                    <div class="mt-2">
+                        <input required type="text" class="col-4" placeholder="Teléfono Fijo">
+                        <input required type="text" class="col-3" placeholder="Celular">
+                        <input required type="text" class="col-4" placeholder="Tel alternativo">
+                    </div>
+                    
+                </div>
+                <h2 class="mt-5">DATOS ACADÉMICOS</h2>
+                <div action="" class="form-control mx-3" style="border: 1px solid #0000003f; width:355px">
+                    <input type="text" name="" class="form-control mb-1"placeholder="Titulo Intermedio" id="">
+                    <div>
+
+                    </div>
+                    <input type="numbre" name="" class="form-control mb-1"placeholder="Año de Egreso" id="">
+                    <input type="numbre" name="" class="form-control mb-1"placeholder="Escuela de egreso" id="">
+                    <input type="numbre" name="" class="form-control mb-1"placeholder="Año de Egreso" id="">
+                </div>
+
+
+
             </div>
         </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </body>
 </html>
+
 
 
 <script>
