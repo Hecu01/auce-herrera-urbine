621bc99393036930548c5e632385c3a790ca373d
Valentin <valentin.urbine967@gmail.com>
2023-07-06 18:34:42 -0300

    18:34hs 06-07-2023 en la facu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,7 +82,7 @@
             
             </div>  
          --}}
-{{--          <div class="tabla">
+         <div class="tabla">
 
             <table class="table table-dark table-striped" style="width:auto">
                <thead>
@@ -121,7 +121,7 @@
 
                </tbody>
             </table>
-         </div> --}}
+         </div>
          
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>      
